e7d2dbb2dce716f0d3cb25cf94b019415abafe66
Yaqing <yaqingj@udel.edu>
2025-10-31 16:46:54 -0400

    add int

--- image_openmp.c
+++ image_openmp.c
@@ -62,10 +62,11 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 void convolute(Image* srcImage, Image* destImage, Matrix algorithm){
     int row,pix,bit;
     #pragma omp parallel for collapse(2) schedule(static) \
-        default(none) shared(srcImage, destImage, algorithm)
+        default(none) shared(srcImage, destImage, algorithm) \
+        private(row, pix, bit)
     for (row = 0; row < srcImage->height; ++row){
         for (pix = 0; pix < srcImage->width; ++pix){
-            for ( bit = 0; bit < srcImage->bpp; ++bit){
+            for (bit = 0; bit < srcImage->bpp; ++bit){
                 destImage->data[Index(pix,row,srcImage->width,bit,srcImage->bpp)] =
                 getPixelValue(srcImage, pix, row, bit, algorithm);
             }
